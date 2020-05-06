c37cadca43eaa78e9e60d38ed5b8abf7211ceb40
Tony Alas <tony_alas99@hotmail.com>
2020-05-06 12:33:23 -0400

    made it better

--- main.py
+++ main.py
@@ -20,15 +20,9 @@ consumer_secret = 'LkuUnE65DuGG5oyzbXKd7M3YEGXNLpyLVwWE33KuRvyGE6F2wV'
 
 oauth_store = {}    # a global dict used to access variables in different app.route's
 
-# this will be used for the delete tweets function
-tweets_global = []
-tweets_username_global = []
-tweets_ids_global = []
-
 @app.route("/")
 def home():
     app_callback_url = url_for('callback', _external=True)
-
     # STEP 1: obtain a request token
     # create an OAuth1 session for signing and posting
     oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
@@ -46,14 +40,6 @@ def home():
     # STEP 2: Get authorization
     # returns the authorization URL with new parameters embedded
     authorization_url = oauth.authorization_url(authorize_url)
-
-    # this is to ensure that the lists from the previous user are cleared
-    if(len(tweets_global) != 0):
-        del tweets_global[:]
-    if(len(tweets_ids_global) != 0):
-        del tweets_ids_global[:]
-    if(len(tweets_username_global) != 0):
-        del tweets_username_global[:]
 
     # return the template with the necessary variables so that the user can click on the Twitter log-in button and go to the authentication page
     return render_template('cancelMe.html', authorize_url=authorize_url, oauth_token=resource_owner_key, request_token_url=request_token_url)
@@ -134,14 +120,6 @@ def callback():
     # from "Integrating premium search twitter developer page"
     endpoint = "https://api.twitter.com/1.1/tweets/search/fullarchive/searchFull.json"
     headers = {"Authorization":"Bearer AAAAAAAAAAAAAAAAAAAAALyrDAEAAAAAmy%2F1oHMkGuirodRGpXt1SorL3mU%3DGN4H2cLxc2jQad2s7yCxNpiOxVRmfwQQ1nnVDmJrgUnkph0prS", "Content-Type": "application/json"} 
-    #--data = '{"query": "can from:tonyalas3"}'
-    #--data = '{"query":' + '"' +badword + ' from:' + screen_name + '"}' # WORKING QUERY
-
-    # fill the global lists with filler items in the first spots so that it can be used easily when deleting tweets
-    # this is because the index received from the html file start counting at 1 instead of 0
-    tweets_global.append("filler")
-    tweets_ids_global.append("filler")
-    tweets_username_global.append("filler")
 
     # list of words in order goes: 
     # lgbtq+ slurs
@@ -152,8 +130,6 @@ def callback():
 
     # the query to search for
     data = '{"query": "(gay OR faggot OR fag OR queer OR homo OR homos OR tranny OR fudgepacker OR sissy OR flamer OR twink OR dyke OR lesbo OR heshe OR shemale OR nig OR nigga OR niggas OR nigger OR nazi OR gook OR chink OR beaner OR coon OR darkie OR goy OR guido OR gypsy OR hick OR kike OR kyke OR niglet OR negro OR nigguh OR niggah OR paki OR polack OR raghead OR towelhead OR spook OR spic OR whitey OR zipperhead OR slut OR whore OR skank OR bitch OR feminazi OR cougar OR prude OR hoe OR butch OR bimbo OR hooker OR wanker OR retard OR cripple OR midget OR retarded OR psycho OR schizo OR spaz OR spastic OR tard OR downy OR kill OR suicide OR kys OR shit OR shitty OR fucking OR motherfucker OR cunt OR bastard OR asshole OR goddamn OR prick OR twat) from:' + screen_name + '", "maxResults": "301", "fromDate": "200603220000"}' # WORKING QUERY (TESTING MULTIPLE TERMS IN ONE REQUEST)
-
-    #data = '{"query": "(gay OR fuck OR nig OR year OR feel OR bad) from:' + screen_name + '", "fromDate": "202003030000"}' # WORKING QUERY (TESTING MULTIPLE TERMS IN ONE REQUEST) DO NOT USE fromDate parameter wit 30-day search otherwise it will break it
 
     nextTokenBool = False
 
@@ -199,10 +175,6 @@ def callback():
         tweets.append(tweet_text) # add username to the corresponding list
         tweets_ids.append(tweet_id) # add the tweet id to the corresponding list
         tweets_usernames.append(tweet_username) # add the tweet's author (username) to the corresponding list
-        # add the same values to the global lists for the delete function at the end of the file
-        tweets_global.append(tweet_text)
-        tweets_ids_global.append(tweet_id)
-        tweets_username_global.append(tweet_username)
         if(len(tweets) == 300):
             break;
 
